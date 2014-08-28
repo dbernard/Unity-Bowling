5510cca50179cc6cefd4579d0c7a924fb6e0119e
Dylan Bernard <dtbernard11@gmail.com>
2014-08-28 16:06:49 -0400

    Implement strikes.
    Add scoring of strikes and testing against strikes.

--- src/bowling_game.c
+++ src/bowling_game.c
@@ -27,15 +27,28 @@ int
 bowling_game_score()
 {
     int score = 0;
+    int all_pins = 10;
 
     int i = 0;
     for (int frame=0; frame<10; frame++)
     {
-        if (rolls[i] + rolls[i+1] == 10)
+        // Strikes
+        if (rolls[i] == all_pins)
+        {
+            // Strike is 10pts + sum of next two rolls
+            score += 10 + rolls[i+1] + rolls[i+2];
+            // Increment the entire FRAME... We rolled a strike, so we're done.
+            i += 1;
+        }
+
+        // Spares
+        else if (rolls[i] + rolls[i+1] == all_pins)
         {
             score += 10 + rolls[i+2];
             i += 2;
         }
+
+        // Other
         else
         {
             score += rolls[i] + rolls[i+1];
--- tests/test-bowling-game.c
+++ tests/test-bowling-game.c
@@ -47,13 +47,44 @@ test_all_ones_game(void)
 }
 
 
-void test_one_spare(void)
+void
+test_one_spare(void)
 {
     bowling_game_roll(5);
     bowling_game_roll(5);
     bowling_game_roll(3);
-    roll_many(17, 0);
+    roll_many(16, 0);
 
     TEST_ASSERT_EQUAL(16, bowling_game_score());
+}
+
+void
+test_one_strike(void)
+{
+    bowling_game_roll(10);
+    bowling_game_roll(3);
+    bowling_game_roll(4);
+    roll_many(17, 0);
+
+    // Strike (10) + 3 + 4
+    TEST_ASSERT_EQUAL(24, bowling_game_score());
+}
+
+void
+test_one_strike_last_frame(void)
+{
+    roll_many(16, 0);
+    bowling_game_roll(10);
+    bowling_game_roll(3);
+    bowling_game_roll(4);
+
+    TEST_ASSERT_EQUAL(24, bowling_game_score());
+}
+
+void test_all_strikes(void)
+{
+    roll_many(12, 10);
+
+    TEST_ASSERT_EQUAL(300, bowling_game_score());
 }
 
